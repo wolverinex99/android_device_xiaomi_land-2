5f4d7bc6c61ffbded9a6db7a0d873f4f6773a92a
JacobDeepu <d2jdeepu@gmail.com>
2019-05-23 19:22:47 +0530

    land:power: Bring in changes from LA.UM.7.6.r1-04800-89xx.0
    Signed-off-by: JacobDeepu <d2jdeepu@gmail.com>

--- device.mk
+++ device.mk
@@ -327,11 +327,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.qcom
-    
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.2-service
 
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
