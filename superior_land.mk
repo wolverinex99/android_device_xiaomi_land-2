29ca18a8f4aa09bb2c0db4b53e533e469a340f45
JacobDeepu <d2jdeepu@gmail.com>
2019-06-06 19:35:00 +0530

    land: Superior OS bringup

--- superior_land.mk
+++ superior_land.mk
@@ -21,8 +21,11 @@ DEVICE_PATH := device/xiaomi/land
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Reloaded-CAF stuff
-$(call inherit-product, vendor/reloaded/common.mk)
+# Inherit some common Superior stuff
+$(call inherit-product, vendor/superior/config/common.mk)
+
+#Boot Animation res
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from land device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -30,7 +33,7 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_land
+PRODUCT_NAME := superior_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
-add_lunch_combo aosp_land-user
-add_lunch_combo aosp_land-userdebug
+add_lunch_combo superior_land-userdebug
